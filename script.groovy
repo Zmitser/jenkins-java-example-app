51269aed466e0e472ac8e6df8eb33c42738bf0f7
Dmitriy <Zmitser@users.noreply.github.com>
2021-10-03 22:28:34 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar (){
 
 def buildImage () {
     echo "Building the docker image"
-    withCredentials([usernamePassword(credentials: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
         sh 'mvn -Pprod jib:dockerBuild -Dimage=zmitser/my-repo:1.0'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push zmitser/my-repo:1.0"
